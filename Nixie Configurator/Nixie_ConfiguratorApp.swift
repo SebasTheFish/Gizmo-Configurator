641b9eb3915895f5c31c7bb5fc4f46f772901376
Sebastian Moruszewicz <sebasmorus@gmail.com>
2024-12-15 18:34:01 -0600

    Initial Commit

---
+++ Nixie Configurator/Nixie_ConfiguratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Nixie_ConfiguratorApp.swift
+//  Nixie Configurator
+//
+//  Created by Sebastian Moruszewicz on 12/15/24.
+//
+
+import SwiftUI
+
+@main
+struct Nixie_ConfiguratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
